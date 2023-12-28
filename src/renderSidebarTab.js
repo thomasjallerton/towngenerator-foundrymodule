ac75939e0fb1a96daf5e7a1864e00ab2a39876c0
Thomas Allerton <thomasjallerton@gmail.com>
2023-12-28 10:46:56 +0000

    v1.2.2: Fix multiple buttons issue.

--- src/renderSidebarTab.js
+++ src/renderSidebarTab.js
@@ -5,11 +5,11 @@ export function renderSidebarTab(app, html) {
     if ((app.options.id === "scenes" || app instanceof SceneDirectory) && game.user.isGM) {
         let button = $("<div class='header-actions action-buttons flexrow'><button>Import from FTG</button></div>");
 
-        button.click(async () => {
+        button.on('click', async () => {
             new ImporterForm().render(true);
         });
 
         $(html).find(".directory-header").find(".header-actions")
-        button.insertAfter($(html).find(".directory-header").find(".header-actions"));
+        button.insertAfter($(html).find(".directory-header").find(".header-actions").last());
     }
 }
